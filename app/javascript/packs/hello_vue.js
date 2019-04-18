a6a8b456b22abb33508e60dc38e6c72faf0bc3cb
hasegawa hirofumi <hase1969.hiro@gmail.com>
2019-04-18 17:50:56 +0900

    hellovueを表示させる

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,18 +5,18 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    el,
-    render: h => h(App)
-  })
-
-  console.log(app)
-})
+// import Vue from 'vue'
+// import App from '../app.vue'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const el = document.body.appendChild(document.createElement('hello'))
+//   const app = new Vue({
+//     el,
+//     render: h => h(App)
+//   })
+//
+//   console.log(app)
+// })
 
 
 // The above code uses Vue without the compiler, which means you cannot
@@ -33,19 +33,22 @@ document.addEventListener('DOMContentLoaded', () => {
 // </div>
 
 
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+console.log('form hello_vue')
 //
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
+document.addEventListener('DOMContentLoaded', () => {
+  if (!document.getElementById('hello')) { return }
+
+  const app = new Vue({
+    el: '#hello',
+    data: {
+      message: "Can you say hello?"
+    },
+    components: { App }
+  })
+})
+
 //
 //
 // If the project is using turbolinks, install 'vue-turbolinks':
